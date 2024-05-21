6f9e06dad2a49e24601afeac6dfcee536d3fb00e
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-22 00:50:03 +0530

    chore: create cors config
    When working with webhooks which are third party apis there is a need to setup cors policies as  nextjs allows request from the same origin.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // match webhook API route
+        source: "/api/webhook",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: process.env.CLIENT },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, stripe-signature",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
