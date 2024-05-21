74dd8a73536af669684208433c4cf74738c39053
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-22 00:48:19 +0530

    feat: create stripe checkout session
    Implement a stripe checkout session and then redirecting the user to the stripe checkout page.

---
+++ src/app/api/checkout_sessions/route.ts
@@ -0,0 +1,69 @@
+import { ProductType } from "@/components/product";
+import { stripe } from "@/lib/stripe";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { cart, email, userId } = await req.json();
+
+    const lineItemsCart = cart.map((item: ProductType) => ({
+      price_data: {
+        currency: "inr",
+        unit_amount: item.price * 100,
+        product_data: {
+          name: item.title,
+          description: item.description,
+          images: [item.image],
+        },
+      },
+      quantity: item.qty,
+    }));
+
+    const shippingRate = await stripe.shippingRates.create({
+      display_name: "Ground shipping",
+      type: "fixed_amount",
+      fixed_amount: {
+        amount: 150,
+        currency: "inr",
+      },
+    });
+
+    // Create Checkout session from body params
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      shipping_address_collection: {
+        allowed_countries: ["IN", "US"],
+      },
+      shipping_options: [
+        {
+          shipping_rate: shippingRate.id,
+        },
+      ],
+      line_items: lineItemsCart,
+      mode: "payment",
+      success_url: `http://localhost:3000/success`,
+      cancel_url: `http://localhost:3000/cart`,
+      metadata: {
+        userId,
+        email,
+        images: JSON.stringify(cart.map((item: ProductType) => item.image)),
+      },
+    });
+
+    return NextResponse.json({ id: session.id });
+  } catch (e: any) {
+    switch (e.type) {
+      case "StripeCardError":
+        console.log(`A payment error occurred: ${e.message}`);
+        break;
+      case "StripeInvalidRequestError":
+        console.log("An invalid request occurred.");
+        break;
+      default:
+        console.log("Another problem occurred, maybe unrelated to Stripe.");
+        break;
+    }
+    console.log(e);
+    return NextResponse.json(e);
+  }
+}
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,6 +4,9 @@ import CartProduct from "@/components/cart-product";
 import FormatPrice from "@/components/format-price";
 import { useGetUser } from "@/hooks/useGetUser";
 import { useCartStore } from "@/providers/couter-store-provider";
+import getStripe from "@/utils/get-stripe";
+import axios from "axios";
+import { signIn } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 
 const CartPage = () => {
@@ -22,6 +25,29 @@ const CartPage = () => {
     );
   }, [cart]);
 
+  const createCheckoutSession = async () => {
+    const stripe = await getStripe();
+
+    const checkoutSession = await axios.post("/api/checkout_sessions", {
+      cart,
+      email: user?.email,
+      userId: user?.id,
+    });
+
+    if (!checkoutSession) return null;
+    const res = await stripe?.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (res?.error) {
+      alert(res.error.message);
+    }
+  };
+
+  const handleCheckout = () => {
+    user ? createCheckoutSession() : signIn();
+  };
+
   if (!user) {
     return <p className="text-2xl font-bold">Please Login to continue</p>;
   }
@@ -32,7 +58,16 @@ const CartPage = () => {
 
   return (
     <div>
-      <FormatPrice price={total as number} />
+      <div className="flex items-center gap-x-8 px-10 pt-8">
+        <FormatPrice price={total as number} />
+        <button
+          className="px-4 py-2 bg-amber-400 text-black hover:bg-amber-500 rounded"
+          onClick={handleCheckout}
+        >
+          {user ? "Checkout" : "Log in to Checkout"}
+        </button>
+      </div>
+
       <section className="grid grid-cols-1 sm:grid-cols-2">
         {cart.map((product) => (
           <CartProduct product={product} key={product.id} />
