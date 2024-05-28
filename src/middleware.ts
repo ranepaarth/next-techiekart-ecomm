3486d4a5022745d2a36287e1e45b868056286476
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-29 02:19:17 +0530

    feat: implement route protection
    Protecting routes from users who are not logged in. This is done so that any sensitive information  won't get disclosed.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,48 @@
 import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
+import { NextResponse } from "next/server";
+import {
+  apiAuthPrefix,
+  apiWebhookPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from "./routes";
 
-export const { auth: middleware } = NextAuth(authConfig);
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+  const res = NextResponse;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isApiWebhookPrefix = nextUrl.pathname.startsWith(apiWebhookPrefix);
+  const isPublicRoutes = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoutes = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute || isApiWebhookPrefix) return;
+
+  if (isAuthRoutes) {
+    if (isLoggedIn) {
+      return res.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoutes) {
+    return res.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return;
+});
+
+export const config = {
+  //from Clerk middleware documentation
+  matcher: [
+    "/((?!.*\\..*|_next).*)", // Don't run middleware on static files
+    "/", // Run middleware on index page
+    "/(api|trpc)(.*)",
+  ], // Run middleware on API routes
+};
---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+export const publicRoutes = ["/", "/cart"];
+
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+export const apiAuthPrefix = "/api/auth";
+
+export const apiWebhookPrefix = "/api/webhook";
+
+export const DEFAULT_LOGIN_REDIRECT = "/";
