a0a25d8d82f7d9339d32532c89d01ff86f7ee5a9
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-29 02:15:13 +0530

    feat: credentials signin
    Implement credentials signin, this includes validating form fields, handling edge case of users loggin in via OAuth provider, checking for existing user, matched password, etc.

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,10 +1,36 @@
+import bcrypt from "bcryptjs";
 import { NextAuthConfig } from "next-auth";
-import GoogleProvider from 'next-auth/providers/google'
+import Credentials from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { getDbUserByEmail } from "./lib/db-user";
+import { LoginSchema } from "./schema";
 export default {
-    providers: [
-        GoogleProvider({
-          clientId: process.env.AUTH_GOOGLE_ID,
-          clientSecret: process.env.AUTH_GOOGLE_SECRET,
-        }),
-      ],
-} satisfies NextAuthConfig
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getDbUserByEmail(email);
+
+          if (!user || !user?.password) return null;
+
+          const matchedPassword = await bcrypt.compare(
+            password,
+            user?.password
+          );
+
+          if (matchedPassword) return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,9 @@ import { prisma } from "./lib/db";
 import { getDbUserById } from "./lib/db-user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+  },
   events: {
     async linkAccount({ user }) {
       await prisma.user.update({
@@ -18,8 +21,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
   callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") return true;
+
+      const existingUser = await getDbUserById(user.id as string);
+
+      if (!existingUser) return false;
+
+      console.log({ existingUser });
+      return true;
+    },
     async session({ token, session }) {
-      // console.log({ token });
       if (!session.user || !token.sub) return session;
 
       if (token.sub && session.user) {
